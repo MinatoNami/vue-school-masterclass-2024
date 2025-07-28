6f21b8f5613aef95ef23a74e39c64100ce754383
Lionel <minato.nami.dev@gmail.com>
2025-07-29 01:42:15 +0800

    Create new migrations, Tasks table and seeded db with values

---
+++ src/pages/projects/tasks/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Tasks Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
+
+const tasks = ref<Tables<'tasks'>[] | null>(null)
+
+;(async () => {
+  const { data, error } = await supabase.from('tasks').select()
+
+  if (error) console.error('Error fetching tasks:', error)
+  else {
+    console.log('Tasks:', data)
+    tasks.value = data
+  }
+})()
+</script>
+
+<style></style>
