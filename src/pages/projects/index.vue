a5674f5902f831972db76bcbaceb5c3c722043b9
Lionel <minato.nami.dev@gmail.com>
2025-07-29 01:33:33 +0800

    Add supabase db types

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,11 +2,32 @@
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <RouterLink :to="{ name: '/projects/[id]', params: { id: project.id } }">
+          {{ project.name }}
+        </RouterLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.error('Error fetching projects:', error)
+  else {
+    console.log('Projects:', data)
+    projects.value = data
+  }
+})()
 </script>
 
 <style></style>
