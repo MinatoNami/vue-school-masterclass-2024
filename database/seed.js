e260fbf276eb9a2ef287b30edbe2af30930662e6
Lionel <minato.nami.dev@gmail.com>
2025-07-29 00:46:22 +0800

    Add fakerjs package and setup run command

---
+++ database/seed.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+
+import { faker } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+console.log(supabase)
+// console.log(import.meta.env.VITE_SUPER_SECRET_KEY)
