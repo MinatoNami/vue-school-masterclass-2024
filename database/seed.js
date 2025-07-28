bc8a3f7d8d71ad691b33c4264892c36b2c562bd7
Lionel <minato.nami.dev@gmail.com>
2025-07-29 01:05:43 +0800

    Add command to add multiple entries via seeding

--- database/seed.js
+++ database/seed.js
@@ -6,15 +6,19 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const seedProject = async () => {
-  const name = faker.lorem.words(3)
-  await supabase.from('projects').insert({
-    name: name,
-    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
-    status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    collaborators: faker.helpers.arrayElements([1, 2, 3]),
-  })
+const seedProject = async (numEntries) => {
+  const projects = []
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+    projects.push({
+      name: name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    })
+  }
+  await supabase.from('projects').insert(projects)
 }
 
-await seedProject()
+await seedProject(10)
 // console.log(import.meta.env.VITE_SUPER_SECRET_KEY)
